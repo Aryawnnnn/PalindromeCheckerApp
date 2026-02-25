d36d5d3cd28f0ba746ec9c48debd9b38ac7e8f8c
Aryawnnnn <aryanrajop18@gmail.com>
2026-02-25 09:28:19 +0530

    UC1: Add welcome message module

--- src/CheckerApp.java
+++ src/CheckerApp.java
@@ -1,3 +1,10 @@
+import java.util.Scanner;
 public class CheckerApp {
-
+    public static void main(String[] args){
+        System.out.println("====================================");
+        System.out.println("WELCOME");
+        System.out.println("====================================");
+        System.out.println("This application checks whether a word is a palindrome");
+        System.out.println("Stay tuned for upcoming features!!!");
+    }
 }
